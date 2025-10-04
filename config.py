a879117d668c70fec9547f62751f1e03b233b988
mord4 <fedorsmolin2006@gmail.com>
2025-10-05 02:26:02 +0300

    движение мыши с изменением направления

---
+++ config.py
@@ -0,0 +1,5 @@
+width = 1920
+height = 1080
+
+dx = 1
+dy = 1
--- mouserunner.py
+++ mouserunner.py
@@ -1,20 +1,37 @@
-from evdev import UInput, ecodes as e
+from evdev import UInput, InputDevice, list_devices, AbsInfo, ecodes as e
+from config import width, height, dx, dy
 import time
+from random import randint
 
 cap = {
-    e.EV_KEY: [e.BTN_LEFT, e.BTN_RIGHT, e.BTN_MIDDLE],
-    e.EV_REL: [e.REL_X, e.REL_Y, e.REL_WHEEL],  # движение и колёсико
+    e.EV_ABS: [
+        (e.ABS_X, AbsInfo(0, 0, width - 1, 0, 0, 0)),
+        (e.ABS_Y, AbsInfo(0, 0, height - 1, 0, 0, 0)),
+    ],
+    e.EV_KEY: [e.BTN_LEFT, e.BTN_RIGHT],
+    e.EV_REL: [e.REL_WHEEL]
 }
 
-dx = 1
-dy = 1
 
-with UInput(cap, name="mouserunner") as ui:
-	i = 200
-	while (i):
-		i -= 1
-		ui.write(e.EV_REL, e.REL_X, dx)
-		ui.write(e.EV_REL, e.REL_Y, dy)
+with UInput(cap, name="mouserunner", bustype=e.BUS_USB) as ui:
+	def move_to(newx, newy, dx, dy):
+		newx += dx
+		newy += dy
+		ui.write(e.EV_ABS, e.ABS_X, newx)
+		ui.write(e.EV_ABS, e.ABS_Y, newy)
 		ui.syn()
+		return newx, newy
+
+
+	curx = randint(0, width)
+	cury = randint(0, height)
+	i = 1000
+	while i:
+		i -= 1
+		curx, cury = move_to(curx, cury, dx, dy)
+		if curx == 0 or curx == width:
+			dx *= -1
+		if cury == 0 or cury == height:
+			dy *= -1
 		time.sleep(0.02)
 
